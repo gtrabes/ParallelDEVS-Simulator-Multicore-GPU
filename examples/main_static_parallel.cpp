a00d6df5de1afe96fa7c4e517018c8e52b9d8061
Guillermo Trabes <guillermotrabes@gmail.com>
2022-10-06 15:52:43 -0400

    added missing files

---
+++ examples/main_static_parallel.cpp
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2022, Guillermo G. Trabes
+ * Carleton University, Universidad Nacional de San Luis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <iostream>
+#include <chrono>
+#include "../modeling/atomic.hpp"
+#include "../simulation/parallel/static_parallel_root_coordinator.hpp"
+
+using namespace std;
+using hclock=std::chrono::high_resolution_clock;
+
+int main(int argc, char **argv) {
+
+	auto sequential_begin = hclock::now(), parallel_begin = hclock::now(), gpu_begin = hclock::now();
+	auto sequential_end = hclock::now(), parallel_end = hclock::now(), gpu_end = hclock::now();
+
+	// First, we parse the arguments
+	if (argc < 5) {
+		std::cerr << "ERROR: not enough arguments" << std::endl;
+		std::cerr << "    Usage:" << std::endl;
+		std::cerr << "    > devs-cuda ATOMICS_NUMBER OUTPUT_FLOPS_NUMBER TRANSITION_FLOPS SIMULATION_TIME" << std::endl;
+		std::cerr << "        (ATOMICS_NUMBER OUTPUT_FLOPS TRANSITION_FLOPS SIMULATION_TIME must be greater or equel to 1)" << std::endl;
+		return -1;
+	}
+
+	size_t n_atomics = std::stoll(argv[1]);
+	if (n_atomics < 1) {
+		std::cerr << "ERROR: ATOMICS_NUMBER is less than 1 (" << n_atomics << ")" << std::endl;
+		return -1;
+	}
+	size_t output_flops = std::stoll(argv[2]);
+	if (output_flops < 1) {
+		std::cerr << "ERROR: OUTPUT_FLOPS is less than 1 (" << output_flops << ")" << std::endl;
+		return -1;
+	}
+	size_t transition_flops = std::stoll(argv[3]);
+	if (transition_flops < 0) {
+		std::cerr << "ERROR: TRANSITION_FLOPS is less than 0 (" << transition_flops << ")" << std::endl;
+		return -1;
+	}
+	size_t simulation_time = std::stoll(argv[4]);
+	if (transition_flops < 0) {
+		std::cerr << "ERROR: SIMULATION_TIME is less than 0 (" << simulation_time << ")" << std::endl;
+		return -1;
+	}
+
+//	Atomic **atomic_pointers_array;
+	Atomic *atomic_array;
+
+/*
+	 // Allocate Unified Memory -- accessible from CPU or GPU
+	cudaMallocManaged(&atomic_pointers_array, size*sizeof(Atomic*));
+
+	for(size_t i = 0; i < size; i++) {
+		cudaMallocManaged(&atomic_pointers_array[i], sizeof(Atomic));
+	}
+*/
+	// Allocate Unified Memory -- accessible from CPU or GPU
+	//cudaMallocManaged(&atomic_array, n_atomics*sizeof(Atomic));
+	atomic_array = (Atomic*) malloc(n_atomics*sizeof(Atomic));
+
+
+	for(size_t i = 0; i < n_atomics; i++) {
+		atomic_array[i] = Atomic(output_flops, transition_flops);
+	}
+
+	parallel_begin = hclock::now();
+
+	parallel_simulation(n_atomics, atomic_array, simulation_time);
+
+	parallel_end = hclock::now();
+
+	// calculate and print time
+	std::cout << "CPU parallel time: "<< std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1>>>(parallel_end - parallel_begin).count() << std::endl;
+
+	return 0;
+}
---
+++ simulation/sequential/sequential_root_coordinator.hpp
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2022, Guillermo G. Trabes
+ * Carleton University, Universidad Nacional de San Luis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <thread>
+
+void sequential_simulation(size_t n_subcomponents, Atomic* subcomponents, size_t sim_time) {
+	double next_time = 0, last_time = 0;
+
+	while(next_time < sim_time) {
+
+		// Step 1: execute output functions
+		for(size_t i=0; i<n_subcomponents;i++){
+			if (subcomponents[i].get_next_time() == next_time) {
+				subcomponents[i].output();
+			}
+		}
+		//end Step 1
+
+		// Step 2: execute output functions
+		//for(size_t i=0; i<n_subcomponents;i++){
+		//	subcomponents.at[]->collection(timeNext);
+		//}
+		//end Step 2
+
+		//Step 3: execute state transition
+		for(size_t i=0; i<n_subcomponents;i++){
+			if (subcomponents[i].get_next_time() == next_time) {
+				if(subcomponents[i].inports_empty() == true) {
+					subcomponents[i].internal_transition();
+				} else {
+					subcomponents[i].confluent_transition(next_time - last_time);
+				}
+				last_time = next_time;
+				subcomponents[i].last_time = last_time;
+				subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+			} else {
+				if(subcomponents[i].inports_empty() == false){
+					subcomponents[i].external_transition(next_time - last_time);
+					last_time = next_time;
+					subcomponents[i].last_time = last_time;
+					subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+				}
+			}
+			subcomponents[i].clear_ports();
+		}
+		//end Step 3
+
+		//Step 4
+		next_time = subcomponents[0].get_next_time();
+		for(size_t i=1; i<n_subcomponents;i++){
+			if(subcomponents[i].get_next_time() < next_time){
+				next_time = subcomponents[i].get_next_time();
+			}
+		}
+		// end Step 4
+	}//end simulation loop
+}
