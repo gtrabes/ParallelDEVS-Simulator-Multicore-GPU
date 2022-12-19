1334ff63a9203a69ed4a6aecf0eb328a42d47573
Guillermo Trabes <guillermotrabes@gmail.com>
2022-12-19 13:41:20 -0500

    added simulation function with static

--- examples/celldevs_sir/simulation/celldevs_sir_static_parallel_root_coordinator.hpp
+++ examples/celldevs_sir/simulation/celldevs_sir_static_parallel_root_coordinator.hpp
@@ -28,7 +28,7 @@
 #include "omp.h"
 #include "../../../affinity/affinity_helpers.hpp"
 
-void parallel_simulation(size_t n_subcomponents, CellDEVSSirAtomic* subcomponents, size_t* n_couplings, size_t** couplings, size_t sim_time,
+void parallel_simulation2(size_t n_subcomponents, CellDEVSSirAtomic* subcomponents, size_t* n_couplings, size_t** couplings, size_t sim_time,
 	size_t num_threads = std::thread::hardware_concurrency()) {
 
 /*
@@ -108,6 +108,130 @@ void parallel_simulation(size_t n_subcomponents, Atomic* subcomponents, size_t* 
 			local_next_time = subcomponents[0].get_next_time();
 			#pragma omp for schedule(static)
 			for(size_t i=1; i<n_subcomponents;i++){
+				if(subcomponents[i].get_next_time() < local_next_time){
+					local_next_time = subcomponents[i].get_next_time();
+				}
+			}
+			#pragma omp single
+			{
+				next_time = local_next_time;
+			}
+			#pragma omp barrier
+			#pragma omp critial
+			{
+				if(local_next_time < next_time){
+					next_time = local_next_time;
+				}
+			}
+			#pragma omp barrier
+			// end Step 4
+
+		}//end simulation loop
+	}//end parallel region
+}
+
+
+
+void parallel_simulation(size_t n_subcomponents, CellDEVSSirAtomic* subcomponents, size_t* n_couplings, size_t** couplings, size_t sim_time,
+	size_t num_threads = std::thread::hardware_concurrency()) {
+
+/*
+void parallel_simulation(size_t n_subcomponents, Atomic* subcomponents, size_t* n_couplings, size_t couplings[][], size_t sim_time,
+	size_t num_threads = std::thread::hardware_concurrency()) {
+*/
+	double next_time = 0, last_time = 0;
+
+	//create threads
+	#pragma omp parallel num_threads(num_threads)
+	//#pragma omp parallel num_threads(num_threads) shared(next_time, last_time)
+	{
+		size_t tid = omp_get_thread_num();
+
+		#pragma omp critical
+		{
+			pin_thread_to_core(tid);
+		}
+
+		size_t thread_number = omp_get_num_threads();
+
+        //calculate number of elements to compute//
+        size_t local_n_subcomponents = n_subcomponents/thread_number;
+
+        // calculate start position/
+        size_t first_subcomponents = tid*local_n_subcomponents;
+
+        // calculate end position/
+        size_t last_subcomponents;
+
+        if(tid != (thread_number-1)){
+            last_subcomponents = (tid+1)*local_n_subcomponents;
+        } else {
+            last_subcomponents = n_subcomponents;
+        }
+
+		double local_next_time;
+
+		while(next_time < sim_time) {
+
+			// Step 1: execute output functions
+			for(size_t i=first_subcomponents; i<last_subcomponents;i++){
+				if (subcomponents[i].get_next_time() == next_time) {
+					subcomponents[i].output();
+				}
+			}
+			#pragma omp barrier
+			//end Step 1
+
+
+			// Step 2: route messages
+			//#pragma omp for schedule(static)
+			for(size_t i = first_subcomponents; i < last_subcomponents; i++){
+				for(size_t j = 0; j < n_couplings[i]; j++ ){
+					//for (size_t k=0; k < 10; k++){
+						auto index = couplings[i][j];
+						//if(index < n_subcomponents) {
+						auto out_bag = subcomponents[index].get_out_bag();
+						subcomponents[i].insert_in_bag(out_bag);
+					//}
+					//}
+						//subcomponents[i].insert_in_bag(subcomponents[couplings[i][j]].get_out_bag());
+
+					//subcomponents[i].insert_in_bag(out_bag);
+					//subcomponents[i].insert_in_bag(subcomponents[0].get_out_bag());
+				}
+			}
+			#pragma omp barrier
+			//end Step 2
+
+			//Step 3: execute state transition
+			//#pragma omp for schedule(static)
+			for(size_t i=first_subcomponents; i< last_subcomponents;i++){
+				if (subcomponents[i].get_next_time() == next_time) {
+					if(subcomponents[i].inbag_empty() == true) {
+						subcomponents[i].internal_transition();
+					} else {
+						subcomponents[i].confluent_transition(next_time - last_time);
+					}
+					last_time = next_time;
+					subcomponents[i].last_time = last_time;
+					subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+				} else {
+					if(subcomponents[i].inbag_empty() == false){
+						subcomponents[i].external_transition(next_time - last_time);
+						last_time = next_time;
+						subcomponents[i].last_time = last_time;
+						subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+					}
+				}
+				subcomponents[i].clear_bags();
+			}
+			#pragma omp barrier
+			//end Step 3
+
+			//Step 4
+			local_next_time = subcomponents[first_subcomponents].get_next_time();
+			//#pragma omp for schedule(static)
+			for(size_t i=first_subcomponents+1; i<last_subcomponents;i++){
 				if(subcomponents[i].get_next_time() < local_next_time){
 					local_next_time = subcomponents[i].get_next_time();
 				}
