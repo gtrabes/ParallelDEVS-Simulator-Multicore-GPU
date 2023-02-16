3a7ef3093d84866a34baffc938f4bdfd2200d14a
Guillermo Trabes <guillermotrabes@gmail.com>
2023-02-16 15:43:00 -0500

    sir model added

---
+++ examples/celldevs_sir/atomics/celldevs_sir_atomic.hpp
@@ -0,0 +1,231 @@
+/**
+ * Abstract implementation of a DEVS atomic model.
+ * Copyright (C) 2022 Guillermo Trabes
+ * ARSLab - Carleton University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CELL_DEVS_SIR_ATOMIC
+#define CELL_DEVS_SIR_ATOMIC
+
+#include <cmath>
+
+	//! Susceptible-Infected-Recovered state.
+	struct SIRState {
+		int p;     //!< Cell population.
+		double s;  //!< Ratio of susceptible people (from 0 to 1).
+		double i;  //!< Ratio of infected people (from 0 to 1).
+		double r;  //!< Ratio of recovered people (from 0 to 1).
+
+		//! Default constructor function. By default, cells are unoccupied and all the population is considered susceptible.
+		SIRState() : p(1000), s(1), i(0), r(0) {
+		}
+	};
+
+	/**
+	 * @brief DEVS atomic model.
+	 *
+	 * The Atomic class is closer to the DEVS formalism than the AtomicInterface class.
+	 * @tparam S the data type used for representing a cell state.
+	 */
+    class CellDEVSSirAtomic {
+     public:
+    	SIRState state;
+    	SIRState lastState;
+    	double rec;   //!< recovery factor.
+    	double susc;  //!< susceptibility factor.
+    	double vir;   //!< virulence factor.
+
+    	double last_time, next_time;
+    	bool is_inbag_empty;
+    	SIRState in_bag[9];
+    	SIRState out_bag;
+    	size_t num_messages_received;
+    	size_t num_neighbors;
+
+    	CellDEVSSirAtomic(double p, double s, double i, double r){
+
+    		state.p = p;
+    		state.s = s;
+    		state.i = i;
+    		state.r = r;
+
+    		rec = 0.2;
+    		susc = 0.8;
+    		vir = 0.4;
+
+    		next_time = 0;
+
+    		if(s != 1.0 || i != 0.0 || r != 0.0){
+    			next_time++;
+    		}
+
+    		last_time = 0;
+    		is_inbag_empty = true;
+    		num_messages_received = 0;
+    	}
+
+		/**
+		 * Sends a new Job that needs to be processed via the Generator::outGenerated port.
+		 * @param s reference to the current generator model state.
+		 * @param y reference to the atomic model output port set.
+		 */
+		void output() {
+			out_bag = state;
+		}
+
+		/**
+		 * Updates GeneratorState::clock and GeneratorState::sigma and increments GeneratorState::jobCount by one.
+		 * @param s reference to the current generator model state.
+		 */
+		void internal_transition() {
+			if (lastState.s != state.s || lastState.i != state.i || lastState.r != state.r) {
+				lastState = state;
+				next_time = next_time+0.1;
+			}
+		}
+
+		/**
+		 * Updates GeneratorState::clock and GeneratorState::sigma.
+		 * If it receives a true message via the Generator::inStop port, it passivates and stops generating Job objects.
+		 * @param s reference to the current generator model state.
+		 * @param e time elapsed since the last state transition function was triggered.
+		 * @param x reference to the atomic model input port set.
+		 */
+		void external_transition(double e) {
+			auto nextState = localComputation(state, in_bag);
+			if (nextState.s != state.s || nextState.i != state.i || nextState.r != state.r) {
+				lastState = state;
+				state = nextState;
+			}
+		}
+
+
+		SIRState localComputation(SIRState state, SIRState neighborhood[9]) {
+			auto newI = newInfections(state, neighborhood);
+			auto newR = newRecoveries(state);
+
+			// We round the outcome to three decimals:
+			state.r = std::round((state.r + newR) * 1000) / 1000;
+			state.i = std::round((state.i + newI - newR) * 1000) / 1000;
+			state.s = 1 - state.i - state.r;
+			return state;
+		}
+
+		double newInfections(SIRState& state, SIRState neighborhood[9]) {
+			double aux = 0;
+			for (int i=0; i<num_messages_received; i++) {
+				auto s = neighborhood[i];
+				aux += s.i * (double)s.p * 0.111;
+				//aux += s.i * (double)s.p * (double)(1.0/num_neighbors);
+				//aux += s.i * (double)s.p * (double)(0.111);
+			}
+			return state.s * susc * std::min(1., vir * aux / state.p);
+		}
+
+		double newRecoveries(SIRState& state){
+			return state.i * rec;
+		}
+
+
+		/**
+		 * Updates GeneratorState::clock and GeneratorState::sigma.
+		 * If it receives a true message via the Generator::inStop port, it passivates and stops generating Job objects.
+		 * @param s reference to the current generator model state.
+		 * @param e time elapsed since the last state transition function was triggered.
+		 * @param x reference to the atomic model input port set.
+		 */
+		void confluent_transition(double e) {
+			internal_transition();
+			external_transition(e);
+		}
+
+		/**
+		 * It returns the value of GeneratorState::sigma.
+		 * @param s reference to the current generator model state.
+		 * @return the sigma value.
+		 */
+		double time_advance() {
+			return 1.;
+		}
+
+		/**
+		* It returns the value of GeneratorState::sigma.
+		* @param s reference to the current generator model state.
+		* @return the sigma value.
+		*/
+		double get_next_time() {
+			return next_time;
+		}
+
+		/**
+		* It returns the value of GeneratorState::sigma.
+		* @param s reference to the current generator model state.
+		* @return the sigma value.
+		*/
+/*
+		bool inbag_empty() {
+			for(size_t i = 0; i < 9; i++){
+				if(in_bag[i] != -1.0){
+					is_inbag_empty == false;
+				}
+			}
+			return is_inbag_empty;
+		}
+*/
+		bool inbag_empty() {
+			if(num_messages_received == 0){
+				is_inbag_empty = true;
+			} else {
+				is_inbag_empty = false;
+			}
+			return is_inbag_empty;
+		}
+
+		/**
+		* It returns the value of GeneratorState::sigma.
+		* @param s reference to the current generator model state.
+		* @return the sigma value.
+		*/
+		void insert_in_bag(SIRState in_message) {
+			if(num_messages_received < 9){
+				in_bag[num_messages_received] = in_message;
+				num_messages_received++;
+			}
+		}
+
+		/**
+		* It returns the value of GeneratorState::sigma.
+		* @param s reference to the current generator model state.
+		* @return the sigma value.
+		*/
+		SIRState get_out_bag() {
+			return out_bag;
+		}
+
+		void clear_bags() {
+/*			for(size_t i = 0; i < 9; i++) {
+				in_bag[i] = new;
+			}
+			out_bag = 0;
+*/
+			num_messages_received = 0;
+		}
+
+
+
+    };
+
+#endif //CADMIUM_CORE_SIMULATION_ABS_SIMULATOR_HPP_
---
+++ examples/celldevs_sir/simulation/celldevs_sir_static_parallel_root_coordinator.hpp
@@ -0,0 +1,255 @@
+/**
+ * Copyright (c) 2022, Guillermo G. Trabes
+ * Carleton University, Universidad Nacional de San Luis
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <thread>
+#include "omp.h"
+#include "../../../affinity/affinity_helpers.hpp"
+
+void parallel_simulation2(size_t n_subcomponents, CellDEVSSirAtomic* subcomponents, size_t* n_couplings, size_t** couplings, size_t sim_time,
+	size_t num_threads = std::thread::hardware_concurrency()) {
+
+/*
+void parallel_simulation(size_t n_subcomponents, Atomic* subcomponents, size_t* n_couplings, size_t couplings[][], size_t sim_time,
+	size_t num_threads = std::thread::hardware_concurrency()) {
+*/
+	double next_time = 0, last_time = 0;
+
+	//create threads
+	//#pragma omp parallel num_threads(num_threads) shared(next_time, last_time, subcomponents, couplings)
+	#pragma omp parallel num_threads(num_threads) shared(next_time, last_time)
+	{
+		size_t tid = omp_get_thread_num();
+
+		#pragma omp critical
+		{
+			pin_thread_to_core_xeon(tid);
+		}
+
+		double local_next_time;
+
+		while(next_time < sim_time) {
+
+			// Step 1: execute output functions
+			#pragma omp for schedule(static)
+			for(size_t i=0; i<n_subcomponents;i++){
+				if (subcomponents[i].get_next_time() == next_time) {
+					subcomponents[i].output();
+				}
+			}
+			#pragma omp barrier
+			//end Step 1
+
+			// Step 2: route messages
+			#pragma omp for schedule(static)
+			for(size_t i = 0; i < n_subcomponents; i++){
+				for(size_t j = 0; j < n_couplings[i]; j++ ){
+					auto index = couplings[i][j];
+					//if(index < n_subcomponents) {
+						auto out_bag = subcomponents[index].get_out_bag();
+						subcomponents[i].insert_in_bag(out_bag);
+					//}
+
+					//subcomponents[i].insert_in_bag(out_bag);
+					//subcomponents[i].insert_in_bag(subcomponents[0].get_out_bag());
+				}
+			}
+			#pragma omp barrier
+			//end Step 2
+
+			//Step 3: execute state transition
+			#pragma omp for schedule(static)
+			for(size_t i=0; i<n_subcomponents;i++){
+				if (subcomponents[i].get_next_time() == next_time) {
+					if(subcomponents[i].inbag_empty() == true) {
+						subcomponents[i].internal_transition();
+					} else {
+						subcomponents[i].confluent_transition(next_time - last_time);
+					}
+					last_time = next_time;
+					subcomponents[i].last_time = last_time;
+					subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+				} else {
+					if(subcomponents[i].inbag_empty() == false){
+						subcomponents[i].external_transition(next_time - last_time);
+						last_time = next_time;
+						subcomponents[i].last_time = last_time;
+						subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+					}
+				}
+				subcomponents[i].clear_bags();
+			}
+			#pragma omp barrier
+			//end Step 3
+
+			//Step 4
+			local_next_time = subcomponents[0].get_next_time();
+			#pragma omp for schedule(static)
+			for(size_t i=1; i<n_subcomponents;i++){
+				if(subcomponents[i].get_next_time() < local_next_time){
+					local_next_time = subcomponents[i].get_next_time();
+				}
+			}
+			#pragma omp single
+			{
+				next_time = local_next_time;
+			}
+			#pragma omp barrier
+			#pragma omp critial
+			{
+				if(local_next_time < next_time){
+					next_time = local_next_time;
+				}
+			}
+			#pragma omp barrier
+			// end Step 4
+
+		}//end simulation loop
+	}//end parallel region
+}
+
+
+
+void parallel_simulation(size_t n_subcomponents, CellDEVSSirAtomic* subcomponents, size_t* n_couplings, size_t** couplings, size_t sim_time,
+	size_t num_threads = std::thread::hardware_concurrency()) {
+
+/*
+void parallel_simulation(size_t n_subcomponents, Atomic* subcomponents, size_t* n_couplings, size_t couplings[][], size_t sim_time,
+	size_t num_threads = std::thread::hardware_concurrency()) {
+*/
+	double next_time = 0, last_time = 0;
+
+	//create threads
+	#pragma omp parallel num_threads(num_threads)
+	//#pragma omp parallel num_threads(num_threads) shared(next_time, last_time)
+	{
+		size_t tid = omp_get_thread_num();
+
+		#pragma omp critical
+		{
+			pin_thread_to_core(tid);
+		}
+
+		size_t thread_number = omp_get_num_threads();
+
+        //calculate number of elements to compute//
+        size_t local_n_subcomponents = n_subcomponents/thread_number;
+
+        // calculate start position/
+        size_t first_subcomponents = tid*local_n_subcomponents;
+
+        // calculate end position/
+        size_t last_subcomponents;
+
+        if(tid != (thread_number-1)){
+            last_subcomponents = (tid+1)*local_n_subcomponents;
+        } else {
+            last_subcomponents = n_subcomponents;
+        }
+
+		double local_next_time;
+
+		while(next_time < sim_time) {
+
+			// Step 1: execute output functions
+			for(size_t i=first_subcomponents; i<last_subcomponents;i++){
+				if (subcomponents[i].get_next_time() == next_time) {
+					subcomponents[i].output();
+				}
+			}
+			#pragma omp barrier
+			//end Step 1
+
+
+			// Step 2: route messages
+			//#pragma omp for schedule(static)
+			for(size_t i = first_subcomponents; i < last_subcomponents; i++){
+				for(size_t j = 0; j < n_couplings[i]; j++ ){
+					//for (size_t k=0; k < 10; k++){
+						auto index = couplings[i][j];
+						//if(index < n_subcomponents) {
+						auto out_bag = subcomponents[index].get_out_bag();
+						subcomponents[i].insert_in_bag(out_bag);
+					//}
+					//}
+						//subcomponents[i].insert_in_bag(subcomponents[couplings[i][j]].get_out_bag());
+
+					//subcomponents[i].insert_in_bag(out_bag);
+					//subcomponents[i].insert_in_bag(subcomponents[0].get_out_bag());
+				}
+			}
+			#pragma omp barrier
+			//end Step 2
+
+			//Step 3: execute state transition
+			//#pragma omp for schedule(static)
+			for(size_t i=first_subcomponents; i< last_subcomponents;i++){
+				if (subcomponents[i].get_next_time() == next_time) {
+					if(subcomponents[i].inbag_empty() == true) {
+						subcomponents[i].internal_transition();
+					} else {
+						subcomponents[i].confluent_transition(next_time - last_time);
+					}
+					last_time = next_time;
+					subcomponents[i].last_time = last_time;
+					subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+				} else {
+					if(subcomponents[i].inbag_empty() == false){
+						subcomponents[i].external_transition(next_time - last_time);
+						last_time = next_time;
+						subcomponents[i].last_time = last_time;
+						subcomponents[i].next_time = last_time + subcomponents[i].time_advance();
+					}
+				}
+				subcomponents[i].clear_bags();
+			}
+			#pragma omp barrier
+			//end Step 3
+
+			//Step 4
+			local_next_time = subcomponents[first_subcomponents].get_next_time();
+			//#pragma omp for schedule(static)
+			for(size_t i=first_subcomponents+1; i<last_subcomponents;i++){
+				if(subcomponents[i].get_next_time() < local_next_time){
+					local_next_time = subcomponents[i].get_next_time();
+				}
+			}
+			#pragma omp single
+			{
+				next_time = local_next_time;
+			}
+			#pragma omp barrier
+			#pragma omp critial
+			{
+				if(local_next_time < next_time){
+					next_time = local_next_time;
+				}
+			}
+			#pragma omp barrier
+			// end Step 4
+
+		}//end simulation loop
+	}//end parallel region
+}
